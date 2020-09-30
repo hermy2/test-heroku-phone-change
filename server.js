fc2a8a9ae361d9fa1a56ee8bdee3ba87af6f487f
Hermann Ouré <hermann.oure@gmail.com>
2020-09-30 19:50:55 +0200

    FIX server.js

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.get('/getContracts', function(req, res) {
             return;
         }
 
-        conn.query('SELECT Name,Product__c FROM salesforce.Contract WHERE Product__c != null',
+        conn.query('SELECT Id,Name,Product__c FROM salesforce.Contract WHERE product__c != null',
         function(err, result) {
             console.log(result)
             if (err) {
